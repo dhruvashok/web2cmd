1243e6a939614bf69bd53e14ecbef0f38de17acc
Dhruv Ashok <47910811+dhruvashok@users.noreply.github.com>
2019-10-28 23:09:32 -0700

    added last edits for 10/28

--- web2cmd.py
+++ web2cmd.py
@@ -5,8 +5,20 @@ import os
 import json
 import platform
 
-print(open("logo.txt").read())
-
 print(open("README.txt").read())
 
-print(platform.system())
+def user_agreement():
+	resp = input('\n\nDo you agree to the terms listed above? (Y/n)')
+	if(resp == "Y" or resp == "y"):
+	 	init()
+	else:
+	 	print("Exiting now...")
+	 	exit(0)
+
+def init():
+	print(open("logo.txt").read())
+
+	print(platform.system())
+
+if __name__ == "__main__":
+	user_agreement()
