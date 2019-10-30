b34d8f95346e54d6b10310c433a3fcef3be773d6
Dhruv Ashok <47910811+dhruvashok@users.noreply.github.com>
2019-10-29 22:54:22 -0700

    added last changes for 10/29

--- web2cmd.py
+++ web2cmd.py
@@ -1,24 +1,38 @@
 import requests
-import termcolor
-import colorama
 import os
 import json
 import platform
+from termcolor import colored
+import colorama
+import datetime
+
+colorama.init()
+info = "[" + colored("*", "cyan") + "]"
+positive = "[" + colored("+", "green") + "]"
+negative = "[" + colored("-", "red") + "]"
+warning = "[" + colored("!", "yellow") + "]"
 
 print(open("README.txt").read())
 
 def user_agreement():
-	resp = input('\n\nDo you agree to the terms listed above? (Y/n)')
-	if(resp == "Y" or resp == "y"):
+	resp = input(f'\n[!] Do you agree to the terms listed above? (Y/n)')
+	if resp == "Y":
 	 	init()
 	else:
 	 	print("Exiting now...")
 	 	exit(0)
 
 def init():
+
 	print(open("logo.txt").read())
 
-	print(platform.system())
+	print(f"{info} Loading web2cmd for {platform.system()}")
+
+	print(f"{positive} Loaded web2cmd for {platform.system()}")
+
+	d = datetime.datetime.today()
+
+	print(f"{info} Started web2cmd at {d.strftime('%H:%M:%S')} {d.strftime('%b-%d-%Y')}")
 
 if __name__ == "__main__":
 	user_agreement()
